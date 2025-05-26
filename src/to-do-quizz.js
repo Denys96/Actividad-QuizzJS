fd261b4be6546bdea0c76dc1612e7f73ee3b0e86
JonaSeguReymundo <00043823@uca.edu.sv>
2025-05-25 19:14:04 -0600

    Second question

--- src/to-do-quizz.js
+++ src/to-do-quizz.js
@@ -1,6 +1,6 @@
 export const quizData = [
     {
-        number: 11,
+        number: 1,
         question: '¿Qué etiqueta HTML se usa para crear una lista desordenada?',
         options: [
             '&lt;list&gt;',
@@ -9,5 +9,16 @@ export const quizData = [
             '&lt;dl&gt;'
         ],
         correct: 2
+    },
+    {
+        number: 2,
+        question: '¿Qué atributo HTML se utiliza para abrir un enlace en una nueva pestaña?',
+        options: [
+            'target="_blank"',
+            'new-tab="true"',
+            'window="_new"',
+            'open="true"'
+        ],
+        correct: 0
     }
 ]
