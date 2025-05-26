0c0819d188c6ac2b34dd57df916a43f9354b5317
JonaSeguReymundo <00043823@uca.edu.sv>
2025-05-25 19:12:31 -0600

    First question

--- src/to-do-quizz.js
+++ src/to-do-quizz.js
@@ -1,2 +1,13 @@
 export const quizData = [
-    {}]
+    {
+        number: 11,
+        question: '¿Qué etiqueta HTML se usa para crear una lista desordenada?',
+        options: [
+            '&lt;list&gt;',
+            '&lt;ol&gt;',
+            '&lt;ul&gt;',
+            '&lt;dl&gt;'
+        ],
+        correct: 2
+    }
+]
